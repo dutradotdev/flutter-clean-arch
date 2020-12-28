6402c0ab2fda216e5d4ff44b7340d72191229deb
Lucas Dutra <dtrxxl@outlook.com>
2020-12-27 21:28:49 -0300

    password input

---
+++ lib/ui/pages/login/components/password_input.dart
@@ -0,0 +1,23 @@
+import 'package:enquete_dev/ui/pages/pages.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PasswordInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final presenter = Provider.of<LoginPresenter>(context);
+    return StreamBuilder<String>(
+        stream: presenter.passwordErrorStream,
+        builder: (context, snapshot) {
+          return TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Senha',
+                icon: Icon(Icons.lock),
+                errorText:
+                    snapshot.data?.isEmpty == true ? null : snapshot.data),
+            obscureText: true,
+            onChanged: presenter.validatePassword,
+          );
+        });
+  }
+}
